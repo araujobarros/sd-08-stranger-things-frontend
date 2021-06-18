f419edd463d673423a9972fe0eabcc1baf2313d6
Paulo Simões <paulohbsimoes@gmail.com>
2021-06-18 17:01:06 -0300

    Adiciona tag de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 
 import StrangerThings from './components/StrangerThings';
+import DevelopmentTag from './components/DevelopmentTag';
+
+const developmentMode = process.env.REACT_APP_DEVELOPMENT === 'true';
 
 function App() {
   return (
     <div className="App">
+      { developmentMode && <DevelopmentTag /> }
       <StrangerThings />
     </div>
   );
---
+++ src/components/DevelopmentTag/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './styles.module.css';
+
+const Development = () => (
+  <div className={ styles.developmentTagContainer }>
+    <p className={ styles.developmentTag }>Em desenvolvimento</p>
+  </div>
+);
+
+export default Development;

9b7a5c41ffe29af7ef454d6f6c3fd86e875299a8
Rosiele David <rosieleserpa@hotmail.com>
2021-06-19 19:00:28 -0300

    Requisito 9 concluído

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 import StrangerThings from './components/StrangerThings';
 
 function App() {
+  const { REACT_APP_DEVELOPMENT_TAG: development } = process.env;
   return (
     <div className="App">
+      {
+        development && <div className="container"><p>Em desenvolvimento</p></div>
+      }
       <StrangerThings />
     </div>
   );

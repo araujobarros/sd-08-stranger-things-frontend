e25276bacbfcf452874fbffd3c62dea2dbf56991
Ivanildo Cândido <systechtecnologia@gmail.com>
2021-06-20 15:50:17 -0300

    Deploy: Development mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
+
+const developmentMode = process.env.REACT_APP_DEVELOPMENT_MODE === 'true';
 
 function App() {
   return (
     <div className="App">
+      {developmentMode && <p>Em desenvolvimento</p>}
       <StrangerThings />
     </div>
   );

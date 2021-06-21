5889feb18989131f03c3ae7d3bde082f33684df7
Leandro Lachini <leandrolachini@gmail.com>
2021-06-21 17:28:04 -0300

    task 9

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
-const isDev = process.env.REACT_APP_DEVELOPMENT;
+const isDev = boolean(process.env.REACT_APP_DEVELOPMENT);
 
 console.log(isDev);
 console.log(typeof (isDev));

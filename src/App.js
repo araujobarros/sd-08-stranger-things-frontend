709b2c7524fd8a892855feaaf95aef7fbc357285
Daniel <danieldsrd@gmail.com>
2021-06-21 16:31:24 -0300

    App em desenvolvimento

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const APP_DEVELOPMENT = true;
+
 function App() {
   return (
     <div className="App">
+      {APP_DEVELOPMENT && <p>Em desenvolvimento</p>} 
       <StrangerThings />
     </div>
   );

1d0362f420085c69b294ccea46950dbc204c8f51
Bruno Martins <bruno.martins217@gmail.com>
2021-06-21 12:58:32 -0300

    Desafio 3

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 
 import StrangerThings from './components/StrangerThings';
 
+const dev = process.env.REACT_APP_DEV_MODE === true;
+
 function App() {
   return (
     <div className="App">
+      {dev && <p>Em desenvolvimento</p>}
       <StrangerThings />
     </div>
   );

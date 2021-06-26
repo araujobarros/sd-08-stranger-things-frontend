6b60dd498ee09ea1ae239deedf15e946ed54d90c
Ana Karine <anakarine.ti@gmail.com>
2021-06-26 13:40:51 -0300

    modified .env

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
+// const strangerThingsConfig = {
+//   url: 'http://localhost:3002',
+//   timeout: 30000,
+// };
+
+// const upsideDownConfig = {
+//   url: 'http://localhost:3003',
+//   timeout: 30000,
+// };
+
 const strangerThingsConfig = {
-  url: 'http://localhost:3002',
-  timeout: 30000,
+  url: process.env.REACT_APP_HAWKINS_URL,
+  timeout: Number(process.env.REACT_APP_HAWKINS_TIMEOUT),
 };
 
 const upsideDownConfig = {
-  url: 'http://localhost:3003',
-  timeout: 30000,
+  url: process.env.REACT_APP_UPSIDEDOWN_URL,
+  timeout: Number(process.env.REACT_APP_UPSIDEDOWN_TIMEOUT),
 };
 
 const charactersService = new CharactersService(strangerThingsConfig);

43ab8368e5c1ba54596d98e2d7300886c76664a2
Uilton Rocha <itoclick@gmail.com>
2021-06-27 12:04:33 -0300

    sem tag

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -145,7 +145,6 @@ class StrangerThings extends React.Component {
           <div>
             <button type="button" onClick={ this.previousPage }>Anterior</button>
             <button type="button" onClick={ this.nextPage }>Próximo</button>
-            <h1>Em desenvolvimento</h1>
           </div>
         </div>
       </div>
